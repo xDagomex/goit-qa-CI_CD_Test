f12a0f5466071b6a5f36d1f2d0223034cde24e4e
xDagomex <161179024+xDagomex@users.noreply.github.com>
2024-10-30 18:04:26 +0100

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,6 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    specPattern: "cypress/integration/**/*.cy.js"
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
