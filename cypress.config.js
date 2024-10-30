4322805352c58ded8757be58d25e352a7764ca58
xDagomex <161179024+xDagomex@users.noreply.github.com>
2024-10-30 21:07:52 +0100

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    module.exports = defineConfig({ e2e: { specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}', // Point to the new folder }, })
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
